6d6789f0b3eee0d156cbfb7a7ff257e10c46bf07
Ignacio Bejarano <ignbejarano@gmail.com>
2020-04-05 11:34:53 -0300

    [ADD] Wallet update & delete endpoints

--- controllers/wallet.controller.js
+++ controllers/wallet.controller.js
@@ -1,24 +1,51 @@
-const {Wallet} = require('../models');
+const { Wallet } = require("../models");
 
-async function newWallet(req,res){
-    try {
-        const { balance, name, account, currency, userId } = req.body;
-        /* TODO Cambiar userId que luego venga por middleware */
-    
-        const wallet = new Wallet({
-          balance,
-          name,
-          account,
-          currency
-        });
-    
-        await wallet.save();
-        res.json({message: "Billetera guardada!", walletId: wallet._id});
-      } catch (error) {
-        res.status(401).json(error.message);
-      }
+async function newWallet(req, res) {
+  try {
+    const { balance, name, account, currency, userId } = req.body;
+    /* TODO Cambiar userId que luego venga por middleware */
+
+    const wallet = new Wallet({
+      balance,
+      name,
+      account,
+      currency,
+    });
+
+    await wallet.save();
+    res.json({ message: "Billetera guardada!", walletId: wallet._id });
+  } catch (error) {
+    res.status(401).json(error.message);
+  }
+}
+
+async function updateWalletBalance(req, res) {
+  try {
+    const { id } = req.params;
+    const wallet = await Wallet.findById(id);
+    wallet.balance += parseInt(req.body.amount);
+    await wallet.save();
+    res.json("Balance actualizado");
+  } catch (error) {
+    res.json(error.message);
+  }
+}
+
+async function deleteWallet(req, res) {
+  try {
+    const { id } = req.params;
+    const wallet = await Wallet.findByIdAndDelete(id);
+    if(!wallet){
+        throw new Error("ID de Billetera no encontrada")
+    }
+    res.json("Billetera eliminada");
+  } catch (error) {
+    res.json(error.message);
+  }
 }
 
 module.exports = {
-    newWallet
-}
+  newWallet,
+  updateWalletBalance,
+  deleteWallet,
+};
--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post("/new", Wallet.newWallet);
 
+router.put("/:id", Wallet.updateWalletBalance);
+
 module.exports = router;
