7d9723f3a93a72a762e259a04ca853b8575fcfe7
Ignacio Bejarano <ignbejarano@gmail.com>
2020-04-14 19:20:31 -0300

    removed deprecated imports

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
-  faMoneyBillWave,
   faMoneyCheckAlt,
   faChartBar,
   faInfoCircle,
