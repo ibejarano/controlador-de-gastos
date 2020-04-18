d7278cf07b4ecf0ef281457716ef200ec7b81bdc
Ignacio Bejarano <ignbejarano@gmail.com>
2020-04-18 18:12:09 -0300

    fix :id query param

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -11,6 +11,6 @@ router.put('/:id/new-expense', Expense.registerExpense, Wallet.addExpense)
 
 router.put("/:id", Wallet.updateWalletBalance);
 
-router.delete("/id", Wallet.deleteWallet )
+router.delete("/:id", Wallet.deleteWallet )
 
 module.exports = router;
