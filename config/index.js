fe9df8dc4ec9dbbe4b528de4977ab40d99bc95d7
Ignacio Bejarano <ignbejarano@gmail.com>
2020-04-26 11:12:51 -0300

    deploy settings

--- config/index.js
+++ config/index.js
@@ -1,12 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-let PORT;
-
-if(process.env.PORT){
-    PORT = process.env.PORT;
-}
+let PORT = process.env.PORT || 5000;
 
 module.exports = {
-    PORT,
-    MONGO_URI: process.env.MONGO_URI
-}
+  PORT,
+  MONGO_URI: process.env.MONGO_URI,
+};
