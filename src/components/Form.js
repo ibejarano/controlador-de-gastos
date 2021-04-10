2ce293e75d3dd97285b3d9a1ddd7ba434ea6ed59
Ignacio Bejarano <ignbejarano@gmail.com>
2021-04-10 12:04:01 -0300

    added box sizing

---
+++ src/components/Form.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import SubmitButton from "../components/SubmitButton";
+
+const StyledForm = styled.form`
+  background: yellow;
+  display: flex;
+  flex-flow: column nowrap;
+  max-width: 630px;
+  border-radius: 10px;
+  padding: 15px;
+  width: 100%;
+
+  label {
+    font-size: 18px;
+    font-weight: bold;
+    color: ${(props) => props.theme.color.mainBackground};
+  }
+
+  input {
+    margin-bottom: 15px;
+    background: ${(props) => props.theme.color.mainBackground};
+    color: ${(props) => props.theme.color.yellowText};
+    font-size: 0.85em;
+    border: none;
+    border-radius: 0.5em;
+    padding: 0.4em;
+  }
+`;
+
+const Form = ({ onSubmit, isSubmitting, formId, register }) => {
+  const [input, setInput] = useState({
+    username: "",
+    email: "budgets@testnew2",
+    password: "testpass",
+    confPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handlePasswordConfirmation = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <React.Fragment>
+      <StyledForm
+        id={formId}
+        onSubmit={async (e) => {
+          e.preventDefault();
+          onSubmit(input);
+        }}
+      >
+        {register && (
+          <React.Fragment>
+            <label>Nombre de usuario</label>
+            <input
+              name="username"
+              type="text"
+              placeholder="Requerido"
+              onChange={handleChange}
+            />
+          </React.Fragment>
+        )}
+
+        <label>E-mail</label>
+        <input
+          name="email"
+          type="email"
+          placeholder="Requerido"
+          onChange={handleChange}
+          value={input.email}
+        />
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={handleChange}
+          value={input.password}
+        />
+        {register && (
+          <React.Fragment>
+            <label>Repetir password</label>
+            <input
+              name="confPassword"
+              type="password"
+              value={input.confPassword}
+              onChange={handlePasswordConfirmation}
+            />
+          </React.Fragment>
+        )}
+      </StyledForm>
+      <SubmitButton
+        text="Ingresar"
+        fontSize="18px"
+        form={formId}
+        fullWidth
+        isSubmitting={isSubmitting}
+      />
+    </React.Fragment>
+  );
+};
+
+export default Form;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,163 +4,66 @@ import { Redirect } from "react-router-dom";
 import { login, register } from "../helpers/requests";
 import { useUser } from "../context/UserContext";
 
-import SubmitButton from "../components/SubmitButton";
 import Error from "../components/Error";
 import TitleContainer from "../components/TitleContainer";
 import TitleAndSubtitle from "../components/TitleAndSubtitle";
+import Form from "../components/Form";
 
 const FormsContainer = styled.div`
   display: flex;
-  flex-flow: row wrap;
+  flex-flow: column wrap;
   justify-content: space-around;
-
-  .form-container {
-    margin: 0;
-    padding: 0;
-    width: 300px;
-  }
-`;
-
-const StyledForm = styled.form`
-  background: yellow;
-  display: flex;
-  flex-flow: column nowrap;
-  /* max-width: 630px; */
-  border-radius: 10px;
-  padding: 15px;
-  padding-bottom: 0;
-
-  label {
-    font-size: 18px;
-    font-weight: bold;
-    color: ${(props) => props.theme.color.mainBackground};
-  }
-
-  input {
-    margin-bottom: 15px;
-    background: ${(props) => props.theme.color.mainBackground};
-    color: ${(props) => props.theme.color.yellowText};
-    font-size: 0.85em;
-    border: none;
-    border-radius: 0.5em;
-    padding: 0.4em;
-  }
+  align-items: center;
 `;
 
 const LoginPage = () => {
   const [redirect, setRedirect] = useState(null);
   const { dispatch } = useUser();
-  const [input, setInput] = useState({
-    username: "",
-    email: "budgets@testnew2",
-    password: "testpass",
-    confPassword: "",
-  });
+
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState(null);
 
-  const handleChange = (e) => {
-    setInput({ ...input, [e.target.name]: e.target.value });
+  const submitLogin = async (input) => {
+    setIsSubmitting(true);
+    const { data, err } = await login(input);
+    if (err) {
+      setError(err.message || err.response.data.error);
+    } else {
+      dispatch({ type: "set-user", payload: data });
+      setRedirect("/wallets");
+    }
+    setIsSubmitting(false);
   };
 
-  const handlePasswordConfirmation = (e) => {
-    setInput({ ...input, [e.target.name]: e.target.value });
+  const submitRegister = async (input) => {
+    setIsSubmitting(true);
+    const { data, err } = await register(input);
+    if (err) {
+      setError(err);
+    } else {
+      dispatch({ type: "set-user", payload: data });
+      setRedirect("/wallets");
+    }
+    setIsSubmitting(false);
   };
 
   return (
     <React.Fragment>
       <TitleContainer title="Bienvenid@!" />
       <FormsContainer>
-        <div className="form-container">
-          <TitleAndSubtitle title="Login" />
-          <StyledForm
-            id="login-form"
-            onSubmit={async (e) => {
-              e.preventDefault();
-              setIsSubmitting(true);
-              const { data, err } = await login(input);
-              if (err) {
-                setError(err.message || err.response.data.error);
-              } else {
-                dispatch({ type: "set-user", payload: data });
-                setRedirect("/wallets");
-              }
-              setIsSubmitting(false);
-            }}
-          >
-            <label>E-mail</label>
-            <input
-              name="email"
-              type="email"
-              placeholder="Requerido"
-              onChange={handleChange}
-              value={input.email}
-            />
-            <label>Password</label>
-            <input
-              name="password"
-              type="password"
-              onChange={handleChange}
-              value={input.password}
-            />
-          </StyledForm>
-          <SubmitButton
-            text="Ingresar"
-            fontSize="18px"
-            fullWidth
-            form="login-form"
-            isSubmitting={isSubmitting}
-          />
-        </div>
-        <div className="form-container">
-          <TitleAndSubtitle title="No posee cuenta? Registrate!" />
-          <StyledForm
-            id="register-form"
-            onSubmit={async (e) => {
-              e.preventDefault();
-              setIsSubmitting(true);
-              const { data, err } = await register(input);
-              if (err) {
-                setError(err);
-              } else {
-                dispatch({ type: "set-user", payload: data });
-                setRedirect("/wallets");
-              }
-              setIsSubmitting(false);
-            }}
-          >
-            <label>Nombre de usuario</label>
-            <input
-              name="username"
-              type="text"
-              placeholder="Requerido"
-              onChange={handleChange}
-            />
-            <label>E-mail</label>
-            <input
-              name="email"
-              type="email"
-              placeholder="Requerido"
-              onChange={handleChange}
-            />
-            <label>Password</label>
-            <input name="password" type="password" onChange={handleChange} />
-            <label>Repetir password</label>
-            <input
-              name="confPassword"
-              type="password"
-              value={input.confPassword}
-              onChange={handlePasswordConfirmation}
-            />
-          </StyledForm>
-          <SubmitButton
-            form="register-form"
-            text="Unirse"
-            isSubmitting={isSubmitting}
-            fontSize="18px"
-            fullWidth
-          />
-        </div>
+        <TitleAndSubtitle title="Login" />
+        <Form
+          isSubmitting={isSubmitting}
+          onSubmit={submitLogin}
+          formId="login-form"
+        />
+        <TitleAndSubtitle title="No posee cuenta? Registrate!" />
+        <Form
+          isSubmitting={isSubmitting}
+          onSubmit={submitRegister}
+          formId="register-form"
+          register
+        />
         {error && <Error error={error} />}
         {redirect && <Redirect to={redirect} />}
       </FormsContainer>
