ff476c927a28afb8c8d2059e24fae6569df38cee
Ignacio Bejarano <ignbejarano@gmail.com>
2021-04-14 11:56:32 -0300

    update corsOptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,9 @@
 const whitelist =
   process.env.NODE_ENV == "development"
-    ? ["http://192.168.0.153:3000", "http://localhost:3000"]
+    ? ["http://192.168.0.153:3000", "http://localhost:5000"]
     : ["https://new-expenses-manager.herokuapp.com/"];
 
 exports.corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: whitelist ,
   credentials: true,
 };
